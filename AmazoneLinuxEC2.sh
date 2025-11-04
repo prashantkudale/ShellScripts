65522016d83de60e58dd04dd2dff09620d9e93e7
prashantkudale <prashant.kudale1989@gmail.com>
2025-11-04 15:01:48 +0530

    Commit the shell scripts

---
+++ AmazoneLinuxEC2.sh
@@ -0,0 +1,12 @@
+# Below script will execute the Jenkins on Amazone Linux machine.
+# Put this script to Instance User Data while creating Instance
+#!/bin/bash
+sudo yum update -y
+sudo dnf install java-17-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
